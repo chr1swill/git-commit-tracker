ab1d2886bf8cad122bb366b5b49ef2c78bff033d
chr1swill <alexanderwilliams20003@gmail.com>
2024-02-18 12:35:47 -0500

    feat: add ts support

--- app.js
+++ app.js
@@ -1,41 +1,77 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+"use strict"; // Enforces strict mode
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+const app = express();
 
-app.use(logger('dev'));
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.use(logger("dev"));
+
+/**
+ * Middleware to parse JSON bodies.
+ * @type {import('express').RequestHandler}
+ */
 app.use(express.json());
+
+/**
+ * Middleware to parse URL-encoded bodies.
+ * @type {import('express').RequestHandler}
+ */
 app.use(express.urlencoded({ extended: false }));
+
+/**
+ * Middleware to parse cookies.
+ * @type {import('express').RequestHandler}
+ */
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/**
+ * Middleware to serve static files.
+ * @type {Function}
+ */
+app.use(express.static(path.join(__dirname, "public")));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Routes
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+/**
+ * Catch 404 and forward to error handler.
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(
+	/**
+	 * Error handler.
+	 * @param {Error} err
+	 * @param {import('express').Request} req
+	 * @param {import('express').Response} res
+	 * @param {import('express').NextFunction} next
+	 */
+	function (err, req, res, next) {
+		// Set locals, only providing error in development
+		res.locals.message = err.message;
+		res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+		// Render the error page
+		res.status(500);
+		res.render("error");
+	},
+);
 
 module.exports = app;
